bff98a2c8092aa0f821d1c1d86b1739c3ac15ca9
David Lefkon <dlefkon@Davids-MacBook-Air-2.local>
2014-03-25 02:46:47 -0400

    commented wordpress line in composer.json

--- Onlinesoaptest/web/app.php
+++ Onlinesoaptest/web/app.php
@@ -3,10 +3,8 @@
 //use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\ClassLoader\DebugClassLoader;
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-//$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-$loader = new UniversalClassLoader();
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
