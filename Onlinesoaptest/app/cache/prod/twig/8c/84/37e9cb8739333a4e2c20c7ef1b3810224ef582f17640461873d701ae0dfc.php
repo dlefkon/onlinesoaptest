d16344a6a27477ee9c37518c5b9a563e2483c004
David Lefkon <dlefkon@Davids-MacBook-Air-2.local>
2014-04-18 00:38:50 -0400

    fixed analytics prod thing and removed cache and log files from git

---
+++ Onlinesoaptest/app/cache/prod/twig/8c/84/37e9cb8739333a4e2c20c7ef1b3810224ef582f17640461873d701ae0dfc.php
@@ -0,0 +1,61 @@
+<?php
+
+/* OnlinesoaptestBundle:Default:about.html.twig */
+class __TwigTemplate_8c8437e9cb8739333a4e2c20c7ef1b3810224ef582f17640461873d701ae0dfc extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("OnlinesoaptestBundle::layout.html.twig");
+
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "OnlinesoaptestBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_content($context, array $blocks = array())
+    {
+        echo " 
+\t\t\t
+\tI was searching the web one fine day as you are doing today, <br>
+\tand was not able to find an online resource to test some SOAP functionlity I <br>
+\twas building into a PHP application.  <br>
+\tI was unable to find a useable one that didn't have to be downloaded.  <br>
+\tSoo .. I decided to make one.<br>
+\t<br>
+\tThis is kinda still in a beta stage and I would welcome any <a href=\"contact\">feedback</a> you have.<br>
+\t<br>
+\tThanks,
+\t<br>
+\tDavid
+\t\t\t
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "OnlinesoaptestBundle:Default:about.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  28 => 3,);
+    }
+}
